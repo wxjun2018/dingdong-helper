35af9baa1cd231dbfa78df12f08aeb55b4699f28
Wu Xingjun <wxjun2018@gmail.com>
2022-04-14 17:26:58 +0800

    添加日志

--- src/main/java/UserConfig.java
+++ src/main/java/UserConfig.java
@@ -28,16 +28,19 @@ public class UserConfig {
         headers.put("ddmc-city-number", "0101");
         headers.put("ddmc-build-version", "2.82.0");
         headers.put("ddmc-device-id", "osP8I0R-oGc2TSHP3iJF_e4-CZSQ");
-        headers.put("ddmc-station-id", "59e5b5df936edf8b2e8f235b");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        //这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        headers.put("ddmc-station-id", "59e5b5df936edf8b2e8f235b");
         headers.put("ddmc-channel", "applet");
         headers.put("ddmc-os-version", "[object Undefined]");
         headers.put("ddmc-app-client-id", "4");
+        //用户是否登录关键信息 请仔细检查
         headers.put("cookie", "DDXQSESSID=52b926ee04bf7e8e26d1de8f938b9678888");
         headers.put("ddmc-ip", "");
         headers.put("ddmc-longitude", "121.496934");
         headers.put("ddmc-latitude", "31.125958");
         headers.put("ddmc-api-version", "9.49.2");
         headers.put("ddmc-uid", "5e21bb530ae80b620090267a");
+        //有反馈说PC端小程序的UserAgent不能下单 如果是PC端抓包直接使用此Agent即可
         headers.put("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800123c) NetType/WIFI Language/zh_CN");
         headers.put("referer", "https://servicewechat.com/wx1e113254eda17715/422/page-frame.html");
         return headers;
@@ -53,7 +56,8 @@ public class UserConfig {
         body.put("uid", "5e21bb530ae80b620090267a");
         body.put("longitude", "121.496934");
         body.put("latitude", "31.125958");
-        body.put("station_id", "59e5b5df936edf8b2e8f235b");//这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        //这个是站点id 请仔细检查和确认这个参数 进入小程序之后首页左上角是不是你所在的站点 如果不是先选择好站点再抓包 不要把站点搞错了 否则不能下单
+        body.put("station_id", "59e5b5df936edf8b2e8f235b");
         body.put("city_number", "0101");
         body.put("api_version", "9.49.2");
         body.put("app_version", "2.82.0");
@@ -63,6 +67,7 @@ public class UserConfig {
         body.put("sharer_uid", "");
         body.put("openid", "osP8I0R-oGc2TSHP3iJF_e4-CZSQ");
         body.put("h5_source", "");
+        //用户是否登录关键信息 请仔细检查
         body.put("device_token", "WHJMrwNw1k/FKPjcOOgRd+OXBIPzC2uTEHyY1DcWvtceRSUPpeax8up7r8Sjn62ALqzwOb09727QFKu/s+EGCPkrcMx9Q13czdCW1tldyDzmauSxIJm5Txg==1487582755342");
         return body;
     }
